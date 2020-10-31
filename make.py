f91616e83db1f9a9825479e74367a4a26ed5747f
Jiří Alexandrovič <jiri.alexandrovic@gmail.com>
2020-10-31 15:29:06 +0100

    Add pack() and encrypt(own_pwd, usr_pwd=) functons.

--- make.py
+++ make.py
@@ -3,10 +3,11 @@ import os
 import re
 import sys
 import shutil
-import subprocess
 import zipfile
+import subprocess
 
 bin_folder="bin/"
+pic_folder="pics/"
 project_path="./"
 project_name="projekt"
 pdf_name=".pdf"
@@ -39,6 +40,15 @@ def texfile_process(fname, source, dest):
         o = open(dest+fname, "w")
         o.write(text)
         o.close()
+
+
+# Find it all recursively
+def zip_folder(path, folder, zipClass):
+
+    for root, dirs, files in os.walk(os.path.join(path, folder)):
+        for f in files:
+            zipClass.write(os.path.join(root, f), os.path.join(root[len(path):], f))
+
 
 
 
@@ -128,9 +138,52 @@ def pack():
     zip.write(project_path+"titulniStrana.tex","./"+"titulniStrana.tex")
     zip.write(project_path+"podekovani.tex","./"+"podekovani.tex")
 
-    
+    zip_folder(project_path, content_folder, zip)
+    zip_folder(project_path, pic_folder, zip)
 
     zip.close()
+
+
+def encrypt(own_pwd, usr_pwd=""):
+    file_path=project_path+pdf_name
+    if not os.path.exists(file_path):
+        print("Pdf for encryption does not exist.")
+
+    try:
+        import pikepdf
+    except:
+        print("For encryption must be enabled import of pikepdf packge.")
+        print("Exitting encrypt.")
+        return
+
+    permiss=pikepdf.Permissions(accessibility=True,
+                                extract=True,
+                                modify_annotation=True,
+                                modify_assembly=False,
+                                modify_form=False,
+                                modify_other=False,
+                                print_highres=True,
+                                print_lowres=True)
+    encrypt=pikepdf.Encryption(user=usr_pwd, owner=own_pwd, allow=permiss)
+    pdf=pikepdf.Pdf.open(file_path,allow_overwriting_input=True)
+    pdf.save(file_path, encryption=encrypt)
+    pdf.close()
+
+    del pikepdf
+
+
+def cleanup():
+    for f in os.listdir(project_path):
+        if (
+        f.endswith(".aux") or f.endswith(".bcf") or
+        f.endswith(".log") or f.endswith(".out") or
+        f.endswith(".blg") or f.endswith(".toc") or
+        f.endswith(".run.xml")):
+            try:
+                os.remove(os.path.join(project_path, f))
+            except OSError as e:
+                print("Error: %s : %s" % (f, e.strerror))
+
 
 def help():
     print("Unimplemented.")
@@ -138,6 +191,6 @@ def help():
 
 if __name__ == "__main__":
     project_path=os.path.dirname(os.path.abspath(sys.modules['__main__'].__file__))+"/"
-    pdf_name=project_name + pdf_name
+    pdf_name=project_name+pdf_name
 
-    clear()
+    encrypt("own")
